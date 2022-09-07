f17cf5f1b3fbfb4da6fb1571a2b794c5fe3ee563
Nawaz Danish <nawazdanish1996@gmail.com>
2022-09-07 16:13:52 +0530

    updated

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,5 +1,13 @@
 const fetchPosts = async (page, limit) => {
-    // optionally write fetching logic here or somewhere else
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=1&_limit=5`;
+    try{
+        const response = await fetch(API_URL);
+        if(!response.ok) throw Error("Wrong Url");
+        const data = await response.json();
+        return data;
+    }catch(err){
+        console.log(err);
+    }
 }
 
-export { fetchPosts }
+export default fetchPosts;
