dd2a6d1f510ecb85e7a7aad42edfacdbfa5f0f8f
Nawaz Danish <nawazdanish1996@gmail.com>
2022-09-17 08:27:29 +0530

    updated

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from './Button';
 const PaginationButtonsList = ({page, handlePage}) => {
-    const BtnArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const BtnArr = [1, 2, 3, 4, 5];
     return (
         <div className="pagination-buttons-list">
             {
