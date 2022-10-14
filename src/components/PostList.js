b659f2320ece7d4fb50ab03511d939332dd3ee7e
Nawaz Danish <nawazdanish1996@gmail.com>
2022-10-14 12:15:54 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import Post from './Post'
+import React, { useContext } from 'react';
+import { pageContext } from '../Helper/pageContext';
+import { PaginationButtonsList } from './PaginationButtonsList';
+import Loader from './Loader';
+import { Post } from './Post';
 
-const PostList = ({ posts }) => {
-    
+
+const PostList = () => {
+    const { state, dispatch } = useContext(pageContext);
     return (
-        <div className='post-list'>
-            {
-                posts.map(post => <Post key={post.id} title={post.title} body={post.body} />)
-            }
-        </div>
+        <>
+            {state.isLoading ? (<Loader />) : (
+                <>
+                    {state.pageData && Object.entries(state.pageData).map((data, index) => {
+                        return (
+                            <Post
+                                key={index}
+                                title={data[1].title}
+                                body={data[1].body}
+                            />
+                        )
+                    })}
+                </>
+            )}
+            <PaginationButtonsList />
+        </>
     )
 }
 
-export default PostList 
+export { PostList }
