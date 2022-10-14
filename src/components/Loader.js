e727d6a6fadde6d621b714599afeef5a92e2db2f
Nawaz Danish <nawazdanish1996@gmail.com>
2022-10-14 12:14:39 +0530

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const Loader = () => {
+export default function () {
   return (
-    <div className='loader'></div>
+    <div class="loader">Loading...</div>
   )
 }
-
-export default Loader
